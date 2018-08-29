f4105cc365f1571efaab09c454b2dde45acd56e7
Jose Luis Garcia del Castillo <personal@garciadelcastillo.es>
2018-08-29 14:12:23 -0400

    Folder refactoring

--- src/DataToolsGrasshopper/Properties/Resources.Designer.cs
+++ src/DataToolsGrasshopper/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace DataToolsGrasshopper.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DataGatesGrasshopper.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DataToolsGrasshopper.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
