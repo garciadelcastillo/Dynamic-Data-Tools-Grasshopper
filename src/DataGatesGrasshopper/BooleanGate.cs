1b5f1714cbe38dd8701ea2d2cbd7dd5ce8324857
Jose Luis Garcia del Castillo <personal@garciadelcastillo.es>
2018-08-29 09:32:05 -0400

    Basic Boolean Gate

---
+++ src/DataGatesGrasshopper/BooleanGate.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace DataGatesGrasshopper
+{
+    public class BooleanGate : GH_Component
+    {
+        private bool UpdateOutput { get; set; }
+        private bool PreviousData { get; set; }
+
+        public BooleanGate()
+          : base("Boolean Gate", "Boolean Gate",
+              "Triggers an update only if new different data came through.",
+              "Data Gates", "Data")
+        {
+        }
+
+        protected override System.Drawing.Bitmap Icon => null;
+        public override Guid ComponentGuid => new Guid("28bf9d69-0fae-4511-ac67-7ade22697ffa");
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("bool", "bool", "", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("bool", "bool", "", GH_ParamAccess.item);
+        }
+
+        protected override void ExpireDownStreamObjects()
+        {
+            if (UpdateOutput)
+            {
+                Params.Output[0].ExpireSolution(false);
+            }
+        }
+
+        protected override void SolveInstance(IGH_DataAccess access)
+        {
+            access.DisableGapLogic();
+
+            bool currentData = false;
+
+            if (!access.GetData(0, ref currentData)) return;
+            access.SetData(0, currentData);
+            
+            if (UpdateOutput)
+            {
+                UpdateOutput = false;
+                PreviousData = currentData;
+                return;
+            }
+
+            if (PreviousData != currentData)
+            {
+                UpdateOutput = true;
+                PreviousData = currentData;
+
+                var doc = OnPingDocument();
+                doc?.ScheduleSolution(5, Callback);
+            }
+        }
+
+        private void Callback(GH_Document doc)
+        {
+            if (UpdateOutput) ExpireSolution(false);
+        }
+
+
+    }
+}
